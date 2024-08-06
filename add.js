bfcb89a5d8dc9fc182aa1021effb66a16f06b33f
TANNU <161803939+TamannaNag@users.noreply.github.com>
2024-08-06 11:06:55 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,114 @@
+/* TEST CASE 1:
+Input (provided)
+let nums = [1, 2, 3, 4]
+let operations = ["add2", "mul3"]
+Output
+[9, 12, 15, 18]
+*/
+
+// let nums = [1,2,3,4];
+// let add = 2;
+// let mul = 3;
+
+// for (let i = 0; i < nums.length; i++) {
+//    nums[i] = (nums[i] + add) * mul
+// }
+// console.log(nums);
+
+//----------------------------------------------------------------------------------------------
+/*// Input
+let nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// Process
+// Filter out odd numbers and then double the remaining numbers
+
+// Output
+[4, 8, 12, 16]
+ */
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let arr = [];
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i]%2 == 0) {
+       
+//      nums[i] = nums[i]**2;
+//      arr.push(nums[i])
+//     }
+// }
+
+// console.log(arr);
+
+
+//-------------------------------------------------------------------------------------------------
+
+/*// Input
+let str = "hello";
+
+// Output
+"olleh" */
+
+// const input = require("readline-sync");
+// let str = input.question("enter the string :");
+
+// let strs = " ";
+// for (let i = str.length-1; i>=0 ;i--) {
+     
+//     strs += str[i]+" ";
+
+// }
+// console.log(strs);
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [-4, -9 ,2 ,3 , 5 , 0, -6,7];
+
+//     // arr = arr.sort()
+//     arr = arr.sort((o, p) => o - p);
+ 
+    
+
+// console.log(arr); 
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [1,2,3,-8,0,-3,-4];
+// const num = arr.length;
+// for (let i = 0; i < num; i++) {
+//     for(let j = 0; j<num; j++){
+//         if(arr[j]>arr[j+1]){
+//             let tam = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = tam;
+//         }
+//     }
+    
+// }
+// console.log(arr);
+
+//----------------------------------------------------------------------------------------------
+
+
+/////// SLICE ()--------------//////////////////
+
+
+// let tannu = "tamannanag";
+// tannu  = tannu.slice(0,7)
+//  console.log(tannu);
+
+// let numbers = [10, 20, 30, 40, 50];
+// let partNumbers = numbers.slice(1, 4); // [20, 30, 40]
+// console.log(partNumbers);
+
+
+
+
+// let user = ['ted', 'tim' , 'ton', 'sam','sor','sod'];
+// user.splice(1,4, 'hello')
+// console.log(user);
+
+
